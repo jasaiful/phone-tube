7fa8f779c3e43afdb963986d3da1f3330bb07822
TAREK <jasaiful@gmail.com>
2023-09-02 23:40:09 +0600

    final push

--- Scripts/index.js
+++ Scripts/index.js
@@ -7,9 +7,9 @@ const handleCategory = async () => {
     const tabContainer = document.getElementById('tab-container');
     data.data.forEach((category) => {
         const div = document.createElement('div')
-        const active = 
-        div.innerHTML = `
-        <a onclick="handleLoadCard('${category.category_id}')" class="tab rounded bg-slate-300">${category.category}</a>
+        const active =
+            div.innerHTML = `
+        <a onclick="handleLoadCard('${category.category_id}')" class="rounded-lg btn focus:bg-common-bg-color">${category.category}</a>
         `;
         tabContainer.appendChild(div);
 
@@ -18,23 +18,8 @@ const handleCategory = async () => {
     });
 
 };
-const sortingFunc = async (categoryId) => {
-    const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`);
-    const data = await res.json();
-    const sortData = data?.data;
-    const datas = data?.data[5].others.views.slice(-0,-1);
-    datas.parseInt()
-    console.log(datas);
 
-    const sortByTrending = sortData.sort((a, b) =>
-        
-        b.others.views.slice(-0, -1).parseInt(views)  - a.others.views.slice(-0, -1).parseInt(views)
-    );
 
-    console.log(sortByTrending);
-}
-
-sortingFunc(1000);
 const handleLoadCard = async (categoryId) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`);
     const data = await res.json();
@@ -44,11 +29,11 @@ const handleLoadCard = async (categoryId) => {
     cardContainer.innerHTML = '';
     notFound.innerHTML = '';
 
-    data.status  ? 
-    data.data?.forEach((news) => {
-        const div = document.createElement('div')
-        div.innerHTML = `
-        <div class="card w-full h-96 p-1 bg-base-100 shadow-xl">
+    data.status ?
+        data.data?.forEach((news) => {
+            const div = document.createElement('div')
+            div.innerHTML = `
+        <div class="card w-full relative h-96 p-1 bg-base-100 shadow-xl">
                 <figure>
                 <img class="h-96" src=${news?.thumbnail} alt="news" />
                 </figure>
@@ -63,10 +48,10 @@ const handleLoadCard = async (categoryId) => {
                         <h4 class="flex gap-2">
                         ${news?.authors[0]?.profile_name}
                         ${news?.authors[0]?.verified ?
-                          `  <img src="./Resources/1.png"/ class="object-cover px-1 mr-1">`
-                            : 
-                            `<span></span>`
-                        } 
+                    `  <img src="./Resources/1.png"/ class="object-cover px-1 mr-1">`
+                    :
+                    `<span></span>`
+                } 
                         </h4>
                         <p>${news?.others?.views} views</p>
                     </div>
@@ -74,22 +59,22 @@ const handleLoadCard = async (categoryId) => {
             </div>
         
         `
-        
-        cardContainer.appendChild(div);
 
-        // console.log(data.data[0].category_id);
+            cardContainer.appendChild(div);
 
-    })
-    : notFound.innerHTML = `<div class='gird justify-center items-center w-full'>
+            // console.log(data.data[0].category_id);
+
+        })
+        : notFound.innerHTML = `<div class='gird justify-center items-center w-full'>
     <img class='mx-auto' src="./Resources/Icon.png" alt="">
     <div class="mt-5">
         <h1 class='text-3xl text-center'>Oops!! Sorry, There is no content here</h1>
     </div>
 </div>`
 
-   
-    // console.log(data.data);
 
+        // console.log(data.data);
+        // < p class="text bg-black text-white absolute mt-52 ml-36 bg-black-500 rounded" > ${ news?.title }</p>
 
 };
 
