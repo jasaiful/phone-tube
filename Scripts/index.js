ae812f0a55e9acaf89492b946eaff966d272eabd
TAREK <jasaiful@gmail.com>
2023-09-02 09:08:17 +0600

    added card section

--- Scripts/index.js
+++ Scripts/index.js
@@ -21,8 +21,49 @@ const handleCategory = async () => {
 const handleLoadCard = async (categoryId) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`);
     const data = await res.json();
-    console.log(data);
-}
+
+    const cardContainer = document.getElementById('card-container');
+
+    data.data?.forEach((news) => {
+        console.log(news.authors[0].verified);
+        const div = document.createElement('div')
+        div.innerHTML = `
+        <div class="card w-full h-96 p-1 bg-base-100 shadow-xl">
+                <figure>
+                <img class="h-96" src=${news?.thumbnail} alt="news" />
+                </figure>
+                <div class="flex justify-center gap-5 my-5">
+                    <div class="w-1/6">
+                        <img class="rounded-full" src=${news?.authors[0]?.profile_picture} alt="">
+                    </div>
+                    <div class="w-5/6">
+                        <h2 class="card-title">
+                        ${news?.title}
+                        </h2>
+                        <h4 class="">
+                        ${news?.authors[0]?.profile_name}
+                            <div class="badge badge-secondary">NEW</div>
+                        </h4>
+                        <p>${news?.others?.views} views</p>
+                    </div>
+                </div>
+            </div>
+        
+        `;
+        cardContainer.appendChild(div);
+
+        // console.log(data.data[0].category_id);
+
+    });
+
+
+
+
+    // console.log(data.data);
+
+
+
+};
 
 
 
